f50740c35d7a0dc4efac8cc8c8594f96e7e25314
whrool@163.com <whrool@163.com>
2015-12-04 17:21:39 +0800

    Add url wrapper which has not been implemented

---
+++ net/http/httpdefs.h
@@ -0,0 +1,28 @@
+// The MIT License (MIT)
+//
+// Copyright(c) 2015 huan.wang
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files(the "Software"),
+// to deal in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// 
+
+#pragma once
+
+#include <unordered_map>
+
+namespace net {
+namespace http {
+    typedef std::unordered_multimap<std::string, std::string> Header;
+    typedef std::unordered_multimap<std::string, std::string> Values;
+} // !namespace http
+} // !namespace net
